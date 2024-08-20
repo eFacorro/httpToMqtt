bbdd06e79dda30c718b92579b89307d7bc15c9ff
eFacorro <facorroesteiro@gmail.com>
2024-08-20 22:48:45 +0200

    cors

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const { httpToMqtt } = require("./funciones.js");
 
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'https://botrix.live'
+}));
 
 app.post("/mqtt", httpToMqtt);
 
